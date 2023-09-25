e74f54f1ec0a172e1be9edef62dadbf0261eac30
derobukal <derobukal@gmail.com>
2023-09-25 23:21:05 +0800

    添加注释

--- src/main/java/com/nosuchfield/jinx/TestJinx.java
+++ src/main/java/com/nosuchfield/jinx/TestJinx.java
@@ -2,6 +2,10 @@ package com.nosuchfield.jinx;
 
 import org.junit.Test;
 
+/**
+ * 编译到Java字节码
+ * <a href="http://jakubdziworski.github.io/enkel/2016/04/05/enkel_7_methods.html">Creating JVM language</a>
+ */
 public class TestJinx {
 
     @Test
--- src/main/java/com/nosuchfield/jinx/Utils.java
+++ src/main/java/com/nosuchfield/jinx/Utils.java
@@ -2,6 +2,12 @@ package com.nosuchfield.jinx;
 
 public class Utils {
 
+    /**
+     * 移除第一个和最后一个字符
+     *
+     * @param data 原始字符串
+     * @return 处理后的字符串
+     */
     public static String removeFirstAndLastChar(String data) {
         if (data.length() < 2) {
             return data;
